2466aa9d6f53a373cb1d93816afd2e44e3bb5263
Dusan Vukadinovic <vukadinovic.dusan.94@gmail.com>
2026-05-13 10:34:55 +0200

    optimized sorting files after spectrum synthesis

--- sort_folders_standard.sh
+++ sort_folders_standard.sh
@@ -1,98 +1,126 @@
-# bash script to organise files and remove data that is no longer needed
-# run this script after the spectral synthesis and averaging calculations
-# change FOLDERS and FILES entries, and line 86
-
 #!/bin/bash
-FOLDERS=380390nm_vald*sun_ssd_mu10
+#
+# Bash script to organise spectral files and remove data that is no longer needed.
+# Run the script after the spectral synthesis and averaging calculations.
+#
+# Example run:
+#
+#  $ bash sort_folders_standard.sh /absolute/path/to/dir/to/sort_me_1 cube_prefix
+#
+# For sorting multiple folders at the same time:
+#
+#  $ bash sort_folders_standard.sh /absolute/path/to/dir/to/sort_me\* cube_prefix
+#
+# The provided path to folder(s) to sort can be relative. 
+#
+# cube_prefix: the prefix of the cubes used for synthesise (i.e. G2_mh00)
+#
+
+GREEN=`tput setaf 2`
+reset=`tput sgr0`
+
+FOLDERS=$1
+FOLDERS=$(ls -d ${FOLDERS})
+
+CUBE_PREFIX=$2
+
+echo ""
 
 for k in $FOLDERS
 do 
-cd $k 
+  echo "${GREEN}$k${reset}"
+  cd $k
 
-FILES=SSD_set2*
-stopcalc=0
-for f in $FILES
-do
-echo "Processing $f files..."
-filename=$(basename -- "$f")
-number="${filename##*_}"
-echo "and the number is $number "
-cd $f 
-cd OUT_INTEN
-for j in 0 1 2 3 4 5 6 7 8 9
-do 
-if [ -f "av_inten_mu_$j.dat" ]
-then
- echo "file av_inten_mu$j.dat in folder $f is there" >> "files_$f.dat"
-else
-  echo "File av_inten_mu_$j.dat in folder $f is not found" >> "files_$f.dat"
-  stopcalc=1
-fi
-done
-mv files*.dat ../../
-cd ../../
-done
-cat files*.dat > availability.dat
+  FILES=$(ls -d ${CUBE_PREFIX}*)
 
+  stopcalc=0
+  echo "Counting files..."
+  for f in ${FILES}
+  do
+    filename=$(basename -- "$f")
+    number="${filename##*_}"
+    # echo "  cube ID: $number"
+    cd $f
+    cd OUT_INTEN
+    for j in 0 1 2 3 4 5 6 7 8 9
+    # for j in 0
+    do
+      if [ -f "av_inten_mu_$j.dat" ]
+      then
+        echo "File av_inten_mu$j.dat in folder $f is there" >> "files_$f.dat"
+      else
+        echo "File av_inten_mu_$j.dat in folder $f is not found" >> "files_$f.dat"
+        stopcalc=1
+      fi
+    done
+    mv files*.dat ../../
+    cd ../../
+  done
+  cat files*.dat > availability.dat
 
-rm files*.dat
-if [ $stopcalc == 1 ]
-then
-    echo "Some files are missing, going to abort"
-    exit 
-fi 
-####### now move nc cubes to full_nc_cubes folder
-#
-mkdir full_nc_cubes
+  rm files*.dat
+  if [ $stopcalc == 1 ]
+  then
+      echo "Some files are missing, going to abort"
+      echo "Check ${k}/availability.dat for missing files"
+      echo ""
+      continue
+  fi
 
-for f in $FILES
-do
-echo "Processing $f files..."
-cd $f
-mv ./OUT_NC $f
-mv $f ../full_nc_cubes
-cd ../
-done
-#################
-## delete everything that is not needed
-#
-#
-#
-mkdir other_data
-for f in $FILES
-do
-echo "Processing $f files..."
-cd $f
-mv examplef/INPUT/flux.input ../other_data
-rm -r running*
-rm -r examplef
-rm -r CARDS
-rm -r collect
-rm *
-cd ../
-done
-######### move the averages out
-#
-for f in $FILES
-do
-echo "Processing $f files..."
-cd $f
-mv ./OUT_INTEN/* ./
-rmdir OUT_INTEN
-cd ../
-done
+  ###### now move nc cubes to full_nc_cubes folder
+  #
+  if [ ! -d "full_nc_cubes" ]; then
+    mkdir full_nc_cubes
+  fi
 
-mkdir av_files
-mv SSD_set2* av_files
-#
-#save important information for later
-mv executables ./other_data
-mv cut_folder ./other_data
-rm -r error
-rm -r muram
-rm -r ready_cubes*
-rm -r rt_example
-rm *
+  echo "Moving .nc cubes..."
+  for f in $FILES
+  do
+    cd $f
+    mv ./OUT_NC $f
+    mv $f ../full_nc_cubes
+    cd ../
+  done
 
-cd ../
+  ###### move the averages out
+  #
+  if [ ! -d "av_files" ]; then
+    mkdir av_files
+  fi
+
+  echo "Moving the average spectra..."
+  for f in $FILES
+  do
+    cd $f
+    mv ./OUT_INTEN ../av_files/$f
+    mv ./collect/mpsa.wave ../av_files
+    cd ../
+  done
+
+  ###### delete everything that is not needed
+  #
+  if [ ! -d "other_data" ]; then
+    mkdir other_data
+  fi
+
+  echo "Deleting unnecessary files..."
+  for f in $FILES
+  do
+    cd $f
+    mv examplef/INPUT/flux.input ../other_data
+    cd ../
+    rm -rf $f
+  done
+
+  #
+  #save important information for later
+  mv executables ./other_data
+  mv cut_folder ./other_data
+  rm -r error
+  rm -r muram
+  rm -r ready_cubes*
+  rm -r rt_example
+  rm *.*
+
+  cd ../
 done
